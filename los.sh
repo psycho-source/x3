5b406c87c5cf10eb4f10ad938e0599df0995c4ea
Psycho Mods <nishsin26@gmail.com>
2018-01-10 19:31:53 +0530

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gitgit clone https://github.com/psycho-source/android_device_leeco_x3.git -b los ~/pos/device/leeco/x3
+git clone https://github.com/psycho-source/android_device_leeco_x3.git -b los ~/pos/device/leeco/x3
 git clone https://github.com/dotOS-Devices/android_kernel_leeco_x3.git -b dot-n ~/pos/kernel/leeco/x3
 git clone https://github.com/dotOS-Devices/android_vendor_leeco_x3.git -b dot-n ~/pos/vendor/leeco/x3

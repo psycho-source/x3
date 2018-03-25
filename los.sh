0b07ac51f171c6ea462f9c7d395d6050bce24baa
Psycho Mods <nishsin26@gmail.com>
2018-03-25 16:07:52 +0530

    Update los.sh

--- los.sh
+++ los.sh
@@ -2,3 +2,5 @@
 git clone https://github.com/psycho-source/android_device_leeco_x3.git -b Psycho-N ~/pos/device/leeco/x3
 git clone https://github.com/psycho-source/Psycho-Kernel.git -b master ~/pos/kernel/leeco/x3
 git clone https://github.com/VenkatVishalV/proprietary_vendor_leeco.git -b android-7.x ~/pos/vendor/leeco
+cd ~/pos
+sh ~/pos/device/leeco/x3/patches/install.sh

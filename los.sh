db275692037ac6845b4c946afe84db9c0670c3b8
Psycho Mods <nishsin26@gmail.com>
2019-02-16 18:18:50 +0530

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git clone https://github.com/KudProject/device_asus_X00T.git -b lineage-16.0 ~/pos/device/asus/X00TD
+git clone https://github.com/KudProject/device_asus_X00T.git -b lineage-16.0 ~/pos/device/asus/X00T
 git clone https://github.com/KudProject/kernel_asus_sdm660.git -b lineage-16.0 ~/pos/kernel/asus/sdm660
 git clone https://github.com/KudProject/proprietary_vendor_asus.git -b pie ~/pos/vendor/asus

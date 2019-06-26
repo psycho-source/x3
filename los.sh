bfb9d57497a231871b65793a9c153aacd7fc6e0e
Psycho Mods <nishsin26@gmail.com>
2019-06-26 18:26:14 +0530

    Use Psycho sauce by default

--- los.sh
+++ los.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git clone https://github.com/psycho-source/device_asus_X00T.git -b Psycho ~/pos/device/asus/X00T
-git clone https://github.com/KudProject/kernel_asus_sdm660.git -b lineage-16.0 ~/pos/kernel/asus/sdm660
+git clone https://github.com/psycho-source/PsychoKernel_X00T.git -b master ~/pos/kernel/asus/sdm660
 git clone https://github.com/KudProject/proprietary_vendor_asus.git -b pie ~/pos/vendor/asus

dca67d4954709bde4d45e2f79b9fafdaf982f0fd
Psycho Mods <nishsin26@gmail.com>
2019-07-04 16:09:52 +0530

    Switch Vendor tree

--- los.sh
+++ los.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git clone https://github.com/psycho-source/device_asus_X00T.git -b Psycho ~/pos/device/asus/X00T
 git clone https://github.com/psycho-source/PsychoKernel_X00T.git -b master ~/pos/kernel/asus/sdm660
-git clone https://github.com/KudProject/proprietary_vendor_asus.git -b pie ~/pos/vendor/asus
+git clone https://github.com/travarilo/proprietary_vendor_asus.git -b pie ~/pos/vendor/asus
 git clone https://github.com/CMRemix/ZION959-UBER-TOOLCHAINS.git -b aarch64-linux-android-8.x ~/UBERTC
